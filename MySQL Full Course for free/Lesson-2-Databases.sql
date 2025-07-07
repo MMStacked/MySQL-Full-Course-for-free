bee6eb315eb2061fddf1fbbc3154f078c555c2e2
MMStacked <102634576+MMStacked@users.noreply.github.com>
2025-07-07 15:45:26 +0100

    Add files via upload

---
+++ MySQL Full Course for free/Lesson-2-Databases.sql
@@ -0,0 +1,13 @@
+# To create a database called 'myDB'
+#CREATE DATABASE myDB;
+
+# to select the speciafied database, in this case 'myDB'
+#USE myDB;
+
+# To drop a specified database, in this case 'myDB'
+#DROP DATABASE myDB;
+
+# Make the specified database read only. 1 = True, 0 = False
+#ALTER DATABASE myDB READ ONLY = 1;
+
+
